440caf5bf9a338e5fd9353a3b6a2d480b1835fae
JosephVC <josephvcardenas@gmail.com>
2021-02-28 12:45:19 -0800

    added auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello there.</h1>
+        <h1>Hello there.  Now with Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
